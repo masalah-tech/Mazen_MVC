5dbdd0febe9246f8269efcb21428c34558345036
Mazen Salah <Mazen Salah@MAZEN_SALAH>
2023-09-30 08:46:32 +0300

    Add project files.

---
+++ MazenWebApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MazenWebApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}

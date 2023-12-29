c54ab7e6ee5008d0b5e71fc87b69042e8a17b339
Mazen Salah <masalah.tech@gmail.com>
2023-12-29 22:33:23 +0300

    #

--- MazenWebApp/Controllers/CategoryController.cs
+++ MazenWebApp/Controllers/CategoryController.cs
@@ -22,6 +22,7 @@ namespace MazenWebApp.Controllers
         {
             return View();
         }
+
         [HttpPost]
         public IActionResult Create(Category category)
         {
@@ -37,6 +38,7 @@ namespace MazenWebApp.Controllers
             {
                 _context.Categories.Add(category);
                 _context.SaveChanges();
+                TempData["success"] = "Category created successfully";
                 //return RedirectToAction("Index", "Category");
                 return RedirectToAction("Index");
             }
@@ -44,5 +46,93 @@ namespace MazenWebApp.Controllers
             return View();
         }
 
+        public IActionResult Edit(int? id)
+        {
+            if (id == null || id < 1)
+            {
+                return NotFound();
+            }
+
+            var category =
+                _context.Categories
+                .Find(id);
+
+            //var category =
+            //    _context.Categories
+            //    .FirstOrDefault(c => c.Id == id);
+
+            //var category =
+            //    _context.Categories
+            //    .Where(c => c.Id == id)
+            //    .FirstOrDefault();
+
+            if (category == null)
+            {
+                return NotFound();
+            }
+
+            return View(category);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Category category)
+        {
+            if (category.Name == category.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("Name", "Name and Display Order must not be the Same");
+            }
+            if (category.Name != null && category.Name.ToLower() == "test")
+            {
+                ModelState.AddModelError("", "test is an invalid value");
+            }
+            if (ModelState.IsValid)
+            {
+                _context.Categories.Update(category);
+                _context.SaveChanges();
+                TempData["success"] = "Category updated successfully";
+                //return RedirectToAction("Index", "Category");
+                return RedirectToAction("Index");
+            }
+
+            return View();
+        }
+
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id < 1)
+            {
+                return NotFound();
+            }
+
+            var category =
+                _context.Categories
+                .Find(id);
+
+            if (category == null)
+            {
+                return NotFound();
+            }
+
+            return View(category);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        public IActionResult DeletePOST(int? id)
+        {
+            var category =
+                _context.Categories
+                .Find(id);
+
+            if (category == null)
+            {
+                return NotFound();
+            }
+
+            _context.Remove(category);
+            _context.SaveChanges();
+            TempData["success"] = "Category deleted successfully";
+
+            return RedirectToAction("Index");
+        }
     }
 }

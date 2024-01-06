f6d10d586f47cbaa00f91b7e0062edc861611627
Mazen Salah <masalah.tech@gmail.com>
2024-01-06 04:24:09 +0300

    #

--- MazenWebApp/Areas/Admin/Controllers/ProductController.cs
+++ MazenWebApp/Areas/Admin/Controllers/ProductController.cs
@@ -1,5 +1,6 @@
 ﻿using Mazen.DataAccess.Repository.IRepository;
 using MazenWebApp.Models;
+using MazenWebApp.Models.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 
@@ -9,10 +10,13 @@ namespace MazenWebApp.Areas.Admin.Controllers
     public class ProductController : Controller
     {
         private readonly IUnitOfWork _unitOfWork;
+        private readonly IWebHostEnvironment _webHostEnvironment;
 
-        public ProductController(IUnitOfWork unitOfWork)
+        public ProductController(IUnitOfWork unitOfWork, 
+            IWebHostEnvironment webHostEnvironment)
         {
             _unitOfWork = unitOfWork;
+            _webHostEnvironment = webHostEnvironment;
         }
 
         public IActionResult Index()
@@ -24,84 +28,103 @@ namespace MazenWebApp.Areas.Admin.Controllers
             return View(products);
         }
 
-        public IActionResult Create()
+        public IActionResult Upsert(int? id)
         {
-            IEnumerable<SelectListItem> categoryList =
-                _unitOfWork.CategoryRepository
-                .GetAll().Select(c => new SelectListItem
-                {
-                    Text = c.Name,
-                    Value = c.Id.ToString()
-                });
-
-            ViewBag.CategoryList = categoryList;
-
-            return View();
-        }
-
-        [HttpPost]
-        public IActionResult Create(Product product)
-        {
-            if (ModelState.IsValid)
-            {
-                _unitOfWork.ProductRepository.Add(product);
-                _unitOfWork.Save();
-                TempData["success"] = "Product created successfully";
-                return RedirectToAction("Index");
-            }
-
-            return View();
-        }
-
-        public IActionResult Edit(int id)
-        {
-            var product = 
-                _unitOfWork.ProductRepository
-                .Get(p => p.Id == id);
-
-            if (product == null)
-            {
-                return NotFound();
-            }
-
-            return View(product);
-        }
-
-        [HttpPost]
-        public IActionResult Edit(Product product)
-        {
-            //int id = product.Id;
-            //var products = 
-            //    _unitOfWork.ProductRepository
-            //    .GetAll();
-
-            //if (_unitOfWork.ProductRepository
-            //    .GetAll()
-            //    .Any(p => p.Id != product.Id && p.ISBN == product.ISBN)) 
-            //{
-            //    ModelState.AddModelError("ISBN", "Book already exists");
-            //}
-            
-            if (ModelState.IsValid)
-            {
-                _unitOfWork.ProductRepository.Update(product);
-                _unitOfWork.Save();
-                TempData["success"] = "Product updated successfully";
-                return RedirectToAction("Index");
-            }
-
-            //foreach (var productItem in _unitOfWork.ProductRepository.GetAll())
-            //{
-            //    if (productItem.Id != product.Id
-            //        && productItem.ISBN == product.ISBN)
+            //IEnumerable<SelectListItem> categoryList =
+            //    _unitOfWork.CategoryRepository
+            //    .GetAll().Select(c => new SelectListItem
             //    {
-            //        ModelState.AddModelError("ISBN", "Book already exists");
-            //    }
-            //}
+            //        Text = c.Name,
+            //        Value = c.Id.ToString()
+            //    });
 
-            
+            //ViewBag.CategoryList = categoryList;
 
-            return View();
+            //ViewData["CategoryList"] = categoryList;
+
+            var productVM = new ProductVM
+            {
+                CategoryList = 
+                    _unitOfWork.CategoryRepository
+                    .GetAll()
+                    .Select(c => new SelectListItem
+                    {
+                        Text = c.Name,
+                        Value = c.Id.ToString()
+                    }),
+                Product = new Product()
+            };
+
+            if (id != null && id != 0)
+            {
+                // update
+                productVM.Product = 
+                    _unitOfWork.ProductRepository
+                    .Get(p => p.Id == id);
+            }
+
+            return View(productVM);
+        }
+
+        [HttpPost]
+        public IActionResult Upsert(ProductVM productVM, IFormFile? productImg)
+        {
+            if (ModelState.IsValid)
+            {
+                string wwwwRootPath = _webHostEnvironment.WebRootPath;
+
+                if (productImg != null)
+                {
+                    string fileName = Guid.NewGuid().ToString() + Path.GetExtension(productImg.FileName);
+                    string productPath = Path.Combine(wwwwRootPath, @"images/product");
+
+                    if (!string.IsNullOrEmpty(productVM.Product.ImageUrl))
+                    {
+                        // Delete the old image
+                        var oldImagePath = 
+                            Path.Combine(wwwwRootPath, productVM.Product.ImageUrl.TrimStart('/'));
+
+                        if (System.IO.File.Exists(oldImagePath))
+                        {
+                            System.IO.File.Delete(oldImagePath);
+                        }
+                    }
+
+                    using (var fileStream = new FileStream(Path.Combine(productPath, fileName), FileMode.Create))
+                    {
+                        productImg.CopyTo(fileStream);
+                    }
+
+                    productVM.Product.ImageUrl = @"/images/product/" + fileName;
+                }
+
+                if (productVM.Product.Id == 0)
+                {
+                    _unitOfWork.ProductRepository.Add(productVM.Product);
+                    TempData["success"] = "Product created successfully";
+                }
+                else
+                {
+                    _unitOfWork.ProductRepository.Update(productVM.Product);
+                    TempData["success"] = "Product updated successfully";
+                }
+
+                _unitOfWork.Save();
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                productVM.CategoryList =
+                    _unitOfWork.CategoryRepository
+                    .GetAll()
+                    .Select(c => new SelectListItem
+                    {
+                        Text = c.Name,
+                        Value = c.Id.ToString()
+                    });
+            }
+
+            return View(productVM);
         }
 
         public IActionResult Delete(int id)
